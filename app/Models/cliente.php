d241048458b4048510f88c3ead28adaa04a96c3d
eduardoer <edu_6al@hotmail.com>
2023-09-27 19:54:51 -0300

    Model cliente

--- app/Models/cliente.php
+++ app/Models/cliente.php
@@ -4,8 +4,52 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class cliente extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'cliente';
+    protected $primaryKey = 'id_cliente';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'nome',
+        'idade',
+        'cpf',
+        'email',
+        'telefone',
+        'cep',
+
+    ];
+
+    /**
+     * -----------------------------
+     * | Relacionamentos
+     * |
+     * ------------------------------
+     */
+
+     public function Adocao(){
+        return $this->belongsTo(
+            Adocao::class,
+            'id_cliente',
+            'id_cliente'
+        );
+
+    }
+
+        public function Historico_Cliente(){
+        return $this->belongsTo(
+            Historico_Cliente::class,
+            'id_cliente',
+            'id_cliente'
+        );
+     }
+
 }
+
