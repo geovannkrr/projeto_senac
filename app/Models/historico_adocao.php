1f23d3777709f6a58cd56377c667da3067d57752
eduardoer <edu_6al@hotmail.com>
2023-09-27 21:43:35 -0300

    Finalizacao Models

--- app/Models/historico_adocao.php
+++ app/Models/historico_adocao.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class historico_adocao extends Model
+class cliente extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'historico_adocao';
+    protected $primaryKey = 'id_historico_adocao';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    protected $fillable = [
+        'tipo_historico_adocao',
+
+    ];
+
+    /**
+     * -----------------------------
+     * | Relacionamentos
+     * |
+     * ------------------------------
+     */
+
+     public function Adocao(){
+        return $this->belongsTo(
+            Adocao::class,
+            'id_adocao',
+            'id_adocao'
+        );
+
+    }
+
 }
+
+
--- app/Models/historico_cliente.php
+++ app/Models/historico_cliente.php
@@ -4,8 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class historico_cliente extends Model
+class cliente extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+    protected $table = 'historico_cliente';
+    protected $primaryKey = 'id_historico_cliente';
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'data_adocao'
+    ];
+
+    protected $fillable = [
+        'qtde_animais',
+        'tipo_animal',
+        'raca_animal',
+
+    ];
+
+    /**
+     * -----------------------------
+     * | Relacionamentos
+     * |
+     * ------------------------------
+     */
+
+     public function Cliente(){
+        return $this->belongsTo(
+            Cliente::class,
+            'id_cliente',
+            'id_cliente'
+        );
+
+    }
+
 }
+
+
+
