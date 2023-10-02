04171a97a2f4fdcb4da5b476fe47d18d18659337
gomesss01 <jpgomescavalcantii2@gmail.com>
2023-10-02 19:14:57 -0300

    Excluir - Pet

--- resources/views/excluir/excluirPet.blade.php
+++ resources/views/excluir/excluirPet.blade.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>EXCLUIR Pet</title>
+        <title>EXCLUIR PET</title>
 
         <!-- LINKS -->
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
