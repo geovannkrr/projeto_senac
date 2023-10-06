b03f30a90cb6bebd3d56bb804a94b93d5c578209
eduardoer <edu_6al@hotmail.com>
2023-10-05 21:57:10 -0300

    Controller HistoricoCliente finalizado

--- app/Models/historico_adocao.php
+++ app/Models/historico_adocao.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class cliente extends Model
+class historico_adocao extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'historico_adocao';
--- app/Models/porte.php
+++ app/Models/porte.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class cliente extends Model
+class porte extends Model
 {
     use HasFactory, SoftDeletes;
     protected $table = 'porte';
